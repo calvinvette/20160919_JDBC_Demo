940e36e7b665a3567dab2f668915c56def299b0d
calvinvette <calvinvette@gmail.com>
2016-09-21 14:31:59 -0400

    Updated AddressMapper

--- src/com/trivera/jdbc/mapper/AddressMapper.java
+++ src/com/trivera/jdbc/mapper/AddressMapper.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
@@ -24,9 +25,21 @@ public interface AddressMapper {
 			+ "(#{line1}, #{line2}, #{city}, #{state}, #{zip} )";
 	public static final String FIND_BY_CITY_STATE = 
 			"SELECT * FROM address WHERE city = #{city} AND state = #{state}";
+	public static final String RESULT_MAPPER = "addressResultMapper";
+	public static final String UPDATE = "UPDATE address SET "
+			+ "line1 = #{line1}, "
+			+ "line2 = #{line2}, "
+			+ "city = #{city}, "
+			+ "state = #{state}, "
+			+ "zip = #{zip} "
+			+ "WHERE address_id = #{addressId}"
+			;
+	public static final String DELETE = "DELETE FROM address "
+			+ "WHERE address_id = #{addressId}"
+			;
 	
 	@Select(FIND_ALL)
-	@Results(value = {
+	@Results(id = RESULT_MAPPER, value = {
 		@Result(property="addressId", column="ADDRESS_ID"),
 		@Result(property="line1", column="LINE1"),
 		@Result(property="line2", column="LINE2"),
@@ -38,15 +51,7 @@ public interface AddressMapper {
 	List<Address> findAll();
 	
 	@Select(FIND_BY_ID)
-	@Results(value = {
-			@Result(property="addressId", column="ADDRESS_ID"),
-			@Result(property="line1", column="LINE1"),
-			@Result(property="line2", column="LINE2"),
-			@Result(property="city", column="CITY"),
-			@Result(property="state", column="STATE"),
-			@Result(property="zip", column="ZIP_CODE"),
-			
-	})
+	@ResultMap(RESULT_MAPPER)
 	Address findById(Long addressId);
 	
 	@Select(FIND_BY_CITY_STATE)
@@ -58,7 +63,12 @@ public interface AddressMapper {
 	@Options(useGeneratedKeys=true, keyProperty="addressId")
 	void insert(Address address);
 	
-//	@Update
-//	@Delete
+	@Update(UPDATE)
+	public Address update(Address address);
+	
+	
+	@Delete(DELETE)
+	public Address delete(Address address);
+	
 	
 }
